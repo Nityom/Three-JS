08c9054fdae89cd8e51f4318e8edbf211cb468cb
Nityom Tikhe <112824495+Nityom@users.noreply.github.com>
2024-11-26 12:27:49 +0530

    Delete 22-physics directory

--- 22-physics/bundler/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    resolve: {
-      fallback: {
-        "util": require.resolve("util/"),
-        // Add other fallbacks as needed, like "path" or "stream"
-      },
-    },
-  };
-  
